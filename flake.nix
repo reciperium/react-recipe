336b3cfe320fe31b7bf4fb78844dc0546a0137c2
Santiago Fraire <santiwilly@gmail.com>
2024-08-12 15:08:13 +0200

    build: update flake and cmds in justfile

--- flake.nix
+++ flake.nix
@@ -10,9 +10,11 @@
     gitignore.url = "github:hercules-ci/gitignore.nix";
     gitignore.inputs.nixpkgs.follows = "nixpkgs";
 
+    nix-filter.url = "github:numtide/nix-filter";
+
   };
 
-  outputs = inputs@{ flake-parts, nixpkgs, gitignore, ... }:
+  outputs = inputs@{ flake-parts, nixpkgs, gitignore, nix-filter, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       debug = true;
       imports = [
@@ -21,45 +23,105 @@
 
       systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }:
-        {
-          packages.react-recipe = pkgs.stdenv.mkDerivation {
-            name = "react-recipe";
-            src = gitignore.lib.gitignoreSource ./.;
-            nativeBuildInputs = with pkgs; [
-              just
-              nodejs_20
-            ] ++ lib.optionals stdenv.isDarwin [
-              libiconv
-              darwin.apple_sdk_11_0.frameworks.Cocoa
-              darwin.apple_sdk_11_0.frameworks.CoreServices
-              darwin.apple_sdk_11_0.frameworks.Security
-            ];
-
-            # Normally required by autotools, but in this case we configure env variables for the
-            # upcoming build phase.
+        let
+          nodejs = pkgs.nodejs_20;
+          node_modules = pkgs.stdenv.mkDerivation {
+            name = "node_modules";
+            src = nix-filter {
+              root = ./.;
+              include = [
+                ./package.json
+                ./package-lock.json
+              ];
+            };
+            # HACK: break the nix sandbox so we can fetch the dependencies. This
+            # requires Nix to have `sandbox = relaxed` in its config.
+            __noChroot = true;
             configurePhase = ''
               # NPM writes cache directories etc to $HOME.
               export HOME=$TMP
             '';
-
-            # Compiling phase
+            buildInputs = [ nodejs ];
             buildPhase = ''
-              ${pkgs.nodejs_20.bin}/npm run build
+              ${nodejs}/bin/npm ci
             '';
 
             installPhase = ''
-              mv dist $out
+              mkdir $out
+              mv node_modules $out/node_modules
             '';
+          };
+        in
+        {
+          packages = {
+            react-recipe = pkgs.stdenv.mkDerivation {
+              name = "react-recipe";
+              src = gitignore.lib.gitignoreSource ./.;
+              nativeBuildInputs = with pkgs; [
+                nodejs
+              ] ++ lib.optionals stdenv.isDarwin [
+                libiconv
+                darwin.apple_sdk_11_0.frameworks.Cocoa
+                darwin.apple_sdk_11_0.frameworks.CoreServices
+                darwin.apple_sdk_11_0.frameworks.Security
+              ];
+
+              # Normally required by autotools, but in this case we configure env variables for the
+              # upcoming build phase.
+              configurePhase = ''
+                # NPM writes cache directories etc to $HOME.
+                ln -sf ${node_modules}/node_modules node_modules
+                export HOME=$TMP
+              '';
+
+              # Compiling phase
+              buildPhase = ''
+                ${nodejs}/bin/npm run build
+              '';
+
+              installPhase = ''
+                mv dist $out
+              '';
+            };
+
+            storybook-static = pkgs.stdenv.mkDerivation {
+              name = "react-recipe";
+              src = gitignore.lib.gitignoreSource ./.;
+              nativeBuildInputs = with pkgs; [
+                nodejs
+              ] ++ lib.optionals stdenv.isDarwin [
+                libiconv
+                darwin.apple_sdk_11_0.frameworks.Cocoa
+                darwin.apple_sdk_11_0.frameworks.CoreServices
+                darwin.apple_sdk_11_0.frameworks.Security
+              ];
+
+              # Normally required by autotools, but in this case we configure env variables for the
+              # upcoming build phase.
+              configurePhase = ''
+                # NPM writes cache directories etc to $HOME.
+                ln -sf ${node_modules}/node_modules node_modules
+                export HOME=$TMP
+              '';
+
+              # Compiling phase
+              buildPhase = ''
+                ${nodejs}/bin/npm run build-storybook
+              '';
+
+              installPhase = ''
+                mv dist $out
+              '';
+            };
           };
 
           devenv.shells.default = {
             name = "react-recipe";
-
             packages = with pkgs; [
               just
-              wasm-pack
-              nodejs_20
-            ] ++ lib.optionals stdenv.isDarwin [
+              commitizen
+
+            ] ++ [ nodejs ] ++ lib.optionals stdenv.isDarwin [
               libiconv
               darwin.apple_sdk_11_0.frameworks.Cocoa
               darwin.apple_sdk_11_0.frameworks.CoreServices
--- justfile
+++ justfile
@@ -2,5 +2,14 @@
 build:
     npm run build
 
+# build the storybook static files
 build-storybook:
     npm run build-storybook
+
+# publish the npm package
+publish:
+    npm publish
+
+# increase the version of the npm package
+bump:
+    cz bump --yes

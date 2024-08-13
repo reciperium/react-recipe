736e8de89fe0e152f1ee326aea82ec04f71e36bb
woile <santiwilly@gmail.com>
2024-08-13 06:24:07 +0000

    Deploying to gh-pages from @ reciperium/react-recipe@57d88c292398109fecb9bf5f7ae858dc61bcee2d 🚀

---
+++ assets/Word-WdtEbtqt.js
@@ -0,0 +1,1 @@
+import{j as s}from"./jsx-runtime-CkxqCPlQ.js";function r({children:o}){return s.jsx("span",{className:"mb-1",children:o})}r.__docgenInfo={description:"",methods:[],displayName:"Word"};export{r as W};

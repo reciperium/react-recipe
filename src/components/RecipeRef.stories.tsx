c8a3fef3060d421241a02652a7b63ff98ec0ec77
Santiago Fraire Willemoes <santiwilly@gmail.com>
2024-08-28 14:47:33 +0200

    feat!: add proper RecipeRef

--- src/components/RecipeRef.stories.tsx
+++ src/components/RecipeRef.stories.tsx
@@ -1,6 +1,17 @@
+import { StoryObj } from "@storybook/react";
 import RecipeRef from "./RecipeRef";
 
 export default {
-    component: RecipeRef,
-}
-export const Default = () => (<RecipeRef>Tomato</RecipeRef>)
+  component: RecipeRef,
+};
+// export const Default = () => (<RecipeRef path="woile/oat-milk" baseUrl="https://reciperium.com/">Oat Milk</RecipeRef>)
+type Story = StoryObj<typeof RecipeRef>;
+export const Default: Story = {
+  args: {
+    children: "Oat Milk",
+    path: "woile/oat-milk",
+    baseUrl: "https://reciperium.com/",
+    quantity: "200",
+    unit: "ml",
+  },
+};
--- src/components/RecipeRef.tsx
+++ src/components/RecipeRef.tsx
@@ -1,38 +1,49 @@
-import { useRecipeContext } from "../provider";
 import React, { HTMLProps } from "react";
-import { cn, createTitle } from "../utils";
+import { createTitle } from "../utils";
 
 export type Props = React.PropsWithChildren &
   HTMLProps<HTMLSpanElement> & {
+    path: string;
+    baseUrl: string;
     quantity?: string | null;
     unit?: string | null;
+    tag?: React.ElementType;
   };
 
+/**
+ * Renders a clickable link to a recipe.
+ *
+ * @component
+ * @param {Props} props - The component props.
+ * @param {number} props.quantity - The quantity of the recipe.
+ * @param {string} props.unit - The unit of measurement for the quantity.
+ * @param {React.ReactNode} props.children - The content of the link.
+ * @param {string} props.className - The CSS class name for the link.
+ * @param {string} props.baseUrl - The base URL for the recipe link.
+ * @param {string} props.path - The path to the recipe.
+ * @param {React.ElementType} props.tag - The HTML tag to use for the link.
+ * @returns {React.ReactNode} The rendered link component.
+ */
 export default function RecipeRef({
   quantity,
   unit,
   children,
   className,
+  baseUrl,
+  path,
+  tag: Tag = "a",
 }: Props): React.ReactNode {
-  const { theme } = useRecipeContext();
-  if (theme === "raw") {
-    return (
-      <span
-        tabIndex={0}
-        title={createTitle(quantity, unit)}
-        className={cn(className)}
-      >
-        {children}
-      </span>
-    );
-  }
+  let url = new URL(path, baseUrl);
+
   return (
-    <span
+    <Tag
       tabIndex={0}
+      href={url.toString()}
       title={createTitle(quantity, unit)}
-      className={cn("", className)}
+      className={className}
+      target="_blank noopener noreferrer"
     >
       {children}
-    </span>
+    </Tag>
   );
 }

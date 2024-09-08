5880065626bdbd40722060ab0aeebf021ca66def
Santiago Fraire Willemoes <santiwilly@gmail.com>
2024-09-08 16:02:58 +0200

    fix: change vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -21,14 +21,14 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['react', 'react-dom'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React',
-        },
-      },
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      // output: {
+      //   // Provide global variables to use in the UMD build
+      //   // for externalized deps
+      //   globals: {
+      //     react: 'React',
+      //   },
+      // },
     },
   },
 

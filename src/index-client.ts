38dbd396e095d1e6336127da9e77cc771abd621a
Santiago Fraire Willemoes <santiwilly@gmail.com>
2024-09-16 09:14:07 +0200

    fix!: move CookMode to client
    BREAKING CHANGE: Import CookMode from `client`

--- src/index-client.ts
+++ src/index-client.ts
@@ -1,7 +1,9 @@
-import { useWakeLock } from "./hooks/use-wake-lock";
+import CookMode from "./components/CookMode";
 import { Switch } from "./components/Switch";
+import { useWakeLock } from "./hooks/use-wake-lock";
 
 export {
+  CookMode,
   Switch,
-  useWakeLock,
-}
+  useWakeLock
+};
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
-import CookMode from "./components/CookMode";
 import Ingredient from "./components/Ingredient";
 import Instructions from "./components/Instructions";
 import Material from "./components/Material";
 import RecipeRef from "./components/RecipeRef";
-
 import Timer from "./components/Timer";
 import Word from "./components/Word";
-
 import { ind } from "./utils";
 
 export {
-  CookMode,
   ind,
   Ingredient,
   Instructions,

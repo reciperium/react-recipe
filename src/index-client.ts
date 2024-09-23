1196f523cb9f6577fc48f4ea7d3a47ab33bb94d2
Santiago Fraire Willemoes <santiwilly@gmail.com>
2024-09-23 13:37:02 +0200

    feat: expose some wasm functions

--- src/index-client.ts
+++ src/index-client.ts
@@ -1,9 +1,16 @@
+import { parse, Recipe, WasmParserError } from "@reciperium/recipe-parser-wasm";
 import CookMode from "./components/CookMode";
 import { Switch } from "./components/Switch";
 import { useWakeLock } from "./hooks/use-wake-lock";
 
 export {
   CookMode,
+  parse,
   Switch,
   useWakeLock
+};
+
+export type {
+  Recipe,
+  WasmParserError
 };

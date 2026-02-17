9f5c3e5027d596ab563185297d418e06c8bb5ac4
Santiago Fraire Willemoes <santiwilly@gmail.com>
2026-02-17 10:50:17 +0000

    fix: version configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from "path";
+import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
-import react from '@vitejs/plugin-react-swc'
-import dts from 'vite-plugin-dts'
-import nodeExternals from 'rollup-plugin-node-externals'
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import nodeExternals from "rollup-plugin-node-externals";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,23 +11,24 @@ export default defineConfig({
     react(),
     wasm(),
     dts({
-      rollupTypes: true, tsconfigPath: "./tsconfig.app.json", insertTypesEntry: true,
+      rollupTypes: true,
+      tsconfigPath: "./tsconfig.app.json",
+      insertTypesEntry: true,
     }),
     process.env.STORYBOOK !== "yes" && nodeExternals(),
   ],
   build: {
-    target: 'esnext',
+    target: "esnext",
     lib: {
       entry: {
-        index: resolve(__dirname, 'src/index.ts'),
-        "index-client": resolve(__dirname, 'src/index-client.ts'),
+        index: resolve(__dirname, "src/index.ts"),
+        "index-client": resolve(__dirname, "src/index-client.ts"),
       },
-      name: 'ReactRecipe',
+      name: "ReactRecipe",
       // // the proper extensions will be added
       // fileName: 'react-recipe',
     },
     chunkSizeWarningLimit: 500,
     rollupOptions: {},
   },
-
-})
+});
